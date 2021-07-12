ddd827b258901d8de52bdb828d4fff3043e398e8
Zhou ZheYuan <120108588@qq.com>
2021-07-12 16:31:42 +0800

    打开场景特效

---
+++ Assets/startPanel/Script/StartPanel.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartPanel : MonoBehaviour
+{
+    public Animator left_Ani;
+    public Animator right_Ani;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick_OpenDoor()
+    {
+        if (left_Ani == null || right_Ani == null) return;
+        left_Ani.SetBool("IsOpen",true);
+        right_Ani.SetBool("IsOpen", true);
+    }
+
+    public void OnClick_CloseDoor()
+    {
+        if (left_Ani == null || right_Ani == null) return;
+        left_Ani.SetBool("IsOpen", false);
+        right_Ani.SetBool("IsOpen", false);
+    }
+}
